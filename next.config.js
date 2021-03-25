c7d69a4ba084b1699a57e7601e4eed464bda1ad9
Elmar van Riet <elmar@elmars-mbp.home>
2021-03-26 00:00:26 +0100

    add optimized image provider

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withYaml = require("next-plugin-yaml");
 
 module.exports = withYaml({
   images: {
+    loader: "imgix",
     domains: ["screenshotapi-dot-net.storage.googleapis.com"],
   },
   typescript: {
